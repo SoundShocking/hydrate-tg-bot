22c03b8561aee5407fc64523bc5a6bcbd772a432
SoundShocking <nireemk@gmail.com>
2024-05-01 19:45:47 +0300

    updates

--- src/views/layouts/MainLayout.tsx
+++ src/views/layouts/MainLayout.tsx
@@ -3,12 +3,21 @@ import { Link, Outlet } from "react-router-dom";
 import { MakeOrder } from "@/components/make-order";
 
 import styles from './MainLayout.module.css'
+import { useTelegram } from "@/hooks";
 
 export const MainLayout: FC = () => {
+	const { tg } = useTelegram()
+
+	const handlePayment = () => {
+		tg.openInvoice('https://t.me/$CWgF-WKrkElCCAAAy_4R1KH2BiE')
+	}
+
 	return <>
 		<header>
 			<Link to={ '/' }>header</Link>
 			<Link to={ '/cart' }>cart</Link>
+
+			<button onClick={ handlePayment }>payment</button>
 		</header>
 
 		<main className={ styles.main }>
