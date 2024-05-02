2551433e8583bbf75722cd02425722dfefa95a95
SoundShocking <nireemk@gmail.com>
2024-05-02 18:24:34 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider, useNavigate } from "react-router-dom";
 import { router } from "@/router";
 import { useTelegram } from "@/hooks";
 import { useEffect } from "react";
 
 function App() {
 	const { tg, user, close } = useTelegram()
+	const navigate = useNavigate()
 
 	const onBackClick = () => {
 		console.log('on back click')
 
-		tg.showAlert('test1337')
-
-		alert('1337')
+		// tg.showAlert('test1337')
+		//
+		// alert('1337')
+		navigate(-1)
 	}
 
 	const oInvoiceClose = (event: { slug: string, status: string }) => {
