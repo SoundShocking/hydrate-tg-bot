3997562b38fb48197f05fd59fa2ecc3ddb4099a5
SoundShocking <nireemk@gmail.com>
2024-05-02 18:26:58 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, useNavigate } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from "@/router";
 import { useTelegram } from "@/hooks";
 import { useEffect } from "react";
